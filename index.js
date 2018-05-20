0507962d1dd25ecf10c6f507f17d30d993310ec1
Lara Sanders <larasanders@gmail.com>
2018-05-20 22:01:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const driver = {}
 function updateDriverWithKeyAndValue(driver, key, value) {
   return Object.assign({}, driver, {[key]: value})
 }
-// function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
-//   driver.address = "12 Broadway"
-//   return driver
-// }
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  return Object.assign(driver, {[key]: value})
+}
 // function deleteFromDriverByKey(driver, key) {
 //   const newDriver = driver
 //   delete newDriver.name
