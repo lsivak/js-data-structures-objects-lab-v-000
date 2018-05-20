91003c1adea29dbefd6722b5f44ce5c4d9aa5ff4
Lara Sanders <larasanders@gmail.com>
2018-05-20 23:26:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function deleteFromDriverByKey(driver, key) {
 return newDriver = delete driver.key
 }
 function destructivelyDeleteFromDriverByKey(driver, key) {
-  return driver = delete driver['key']
+  return driver = delete driver[key]
 }
